12cdd9738264a8aec1e2410214236cfa3951b6bb
Tri Le <trile95.it@gmail.com>
2022-06-15 21:29:44 -0400

    Initial Commit

---
+++ DogAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogAPI
+//
+//  Created by Tri Le on 6/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
